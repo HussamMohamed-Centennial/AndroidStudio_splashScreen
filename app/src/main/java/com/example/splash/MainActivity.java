58a00fadaf41c3a1cb1e0e956683ab7c0cb422c8
HussamEldinMohamed <dr.hussam@live.com>
2022-05-04 22:47:11 -0400

    added the splash screen with timer and fading effect

--- app/src/main/java/com/example/splash/MainActivity.java
+++ app/src/main/java/com/example/splash/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.splash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int Splash_Screen_Time_out = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(MainActivity.this,
+                        FinalActivity.class);
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                //Intent is used to switch from one activity to another.
+
+                startActivity(i);
+                //invoke the SecondActivity.
+
+                finish();
+                //the current activity will get finished.
+            }
+        }, Splash_Screen_Time_out);
+
     }
 }
